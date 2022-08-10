5757eb767a79774af6b8df4237fd987b9380732b
shaniet <101319495+shaniet@users.noreply.github.com>
2022-08-10 07:43:25 -0700

    Update StudentWorld.h

--- Projects/TunnelMan/StudentWorld.h
+++ Projects/TunnelMan/StudentWorld.h
@@ -6,23 +6,30 @@
 #include "Actor.h"
 #include <string>
 #include <vector>
-
+#include <cmath>
+#include <sstream>
+#include <iomanip>
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
 class StudentWorld : public GameWorld
 {
 public:
-    StudentWorld(std::string assetDir);
+    StudentWorld(std::string assetDir):GameWorld(assetDir){}
 
     virtual int init();
     virtual int move();
-
     virtual void cleanUp();
+    void removeEarth(int x, int y);
+    void setGridContent(int x, int y, int ID);
+    TunnelMan* gettunnelMan() const;
     virtual ~StudentWorld();
 private:
     TunnelMan* m_tunnelMan;
 	std::vector<Actor*> m_actors;
-	std:: vector<Earth*> m_earth;
+    Earth * m_earth[64][64];
+    int grid[64][64];
+    int numTicks;
 };
 
 #endif // STUDENTWORLD_H_
+
