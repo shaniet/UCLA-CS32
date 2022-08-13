27b45353bca8670c1eba284c915e6758547430e1
shaniet <101319495+shaniet@users.noreply.github.com>
2022-08-13 15:28:54 -0700

    Update Actor.cpp

--- Projects/TunnelMan/Actor.cpp
+++ Projects/TunnelMan/Actor.cpp
@@ -1,97 +1,90 @@
-#include "Actor.h"
-#include "StudentWorld.h"
+#ifndef ACTOR_H_
+#define ACTOR_H_
 
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+#include "GraphObject.h"
+#include "GameConstants.h"
+#include <algorithm>
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+// Base class for Game's objects
+
+class StudentWorld;
+
+class Actor: public GraphObject
+{
+private:
+    bool m_life;
+    StudentWorld* m_world;
+public:
+    //Base class constructor
+    Actor(int imageID, int startX, int startY, StudentWorld* world, Direction dir = right, double size = 1.0, unsigned int depth = 0): GraphObject(imageID,startX,startY,dir,size,depth),m_life(true),m_world(world){setVisible(true);}
+    //base class destructor
+    virtual ~Actor() {}
+    //pure virtual function to be overridden by derived classes
+    void virtual doSomething() = 0;
+    // check if the actor is alive
+    bool isAlive()const { return m_life; }
+    // set the actor's life to false
+    void die() { m_life = false; }
+    // get the student world pointer
+    StudentWorld* getWorld() { return m_world; }
+};
+
+class Earth: public Actor
+{
+public:
+    //Earth constructor
+    Earth(int startX, int startY, StudentWorld* world): Actor(TID_EARTH,startX,startY,world,right,.25,3){}
+   // earth's do something function(doesnt do anything)
+    virtual void doSomething(){}
+     //Earth destructor
+    virtual ~Earth() {}
+};
 
 
-//NOTES;
-//allowed to use isvisible and getid
-//waterpool(same bounds as boulder but can span in the middle shaft(0-60,0-56)
-//gold((0-60,0-56)except shaft)<- same for barrel have the same bounds
-//sonar can spawn upper left
 
-// protestors ticks to wait and num squares between (calculate every single time) in the do something()
-//check if non resting tiks are not greater than 15
-//bfs/dfs advice: midterm had outline for searching algorithm (when you find the path you can create a 2d array of discover points and as you figure out a solution you can mark off the coordinates accordingly
-    //bool/char array of earth and check if visible for earth its essentially a wall
-    //start them at false / dequeing
-//protestor requirment wording pg41 point 5: verticle line of sight, no earth (loop through earth objects)
-//differentia different actors in a vector-> get the id for any actor
-
-void TunnelMan::doSomething(){
-    //if dead, exit function
-    if(!isAlive()){
-        return;
+class TunnelMan: public Actor{
+private:
+    int hitPoints, water, sonar, nugget;
+public:
+    //TunnelMan constructor
+    TunnelMan(StudentWorld* world):Actor(TID_PLAYER,30,60,world),hitPoints(10),water(5),sonar(1),nugget(0){}
+    //TunnelMan doSomething function
+    virtual void doSomething();
+//    virtual void Annoy();
+// public helper functions
+    void sethitPoints(int points){
+        hitPoints = points;
     }
-    //create a switch statment to check for user input
-    int ch=-1;
-    if(getWorld()-> getKey(ch)){
-        switch(ch)
-        {
-            case KEY_PRESS_RIGHT:
-          
-                // check if the tunnelman is within the bounds of the screen
-                if(getX() <60 && getDirection() == right){ 
-                    // if so move the tunnelman right
-                    moveTo(getX() +1, getY());
-                    //remove earth from the screen
-                    getWorld()->removeEarth(getX(), getY());
-
-                }
-                // set the direction to right
-                    setDirection(right);
-         
-                break;
-            case KEY_PRESS_LEFT:
-          
-                // check if the tunnelman is within the bounds of the screen
-                if(getX() >0 && getDirection() == left){
-                    // if so move the tunnelman left
-                    moveTo(getX() -1, getY());
-                    //remove earth from the screen
-                    getWorld()->removeEarth(getX(), getY());
-                }
-                // set the direction to left
-                    setDirection(left);
-                break;
-            case KEY_PRESS_UP:
-          
-                if(getY() <60 && getDirection() == up){
-                    // if so move the tunnelman up
-                    moveTo(getX(), getY()+1);
-                    //remove earth from the screen
-                    getWorld()->removeEarth(getX(), getY());
-                }
-                // set the direction to up
-                    setDirection(up);
-                break;
-            case KEY_PRESS_DOWN:
-           
-                if(getY() >0 && getDirection() == down){
-                    // if so move the tunnelman down
-                    moveTo(getX() , getY()-1);
-                    //remove earth from the screen
-                    getWorld()->removeEarth(getX(), getY());
-
-                }
-                // set the direction to down
-                    setDirection(down);
-               
-                break;
-            case KEY_PRESS_ESCAPE:
-                die();
-//                kill player
-                break;
-            case KEY_PRESS_SPACE:
-                {
-//                    getWorld()->shootWater(getX(), getY());
-                    break;
-                }
-            case 'Z':
-            case 'z':
-                // if (sonar <= 0)
-                    break;
-                // sonar--;
-        }
+    void decWater(){
+        if(water > 0)
+        water--;
     }
-}
+    void incWater(){
+        water+=5;
+    }
+    void incSonar(){
+        sonar++;
+    }
+    //public getter functions: get hit points, get water, get sonar, get nugget
+
+    int getHitPoints() const{
+        return hitPoints;
+    }
+    int getWater() const{
+        return water;
+    }
+    int getSonar() const{
+        return sonar;
+    }
+    int getNugget() const{
+        return nugget;
+    }
+    //TunnelMan destructor
+    virtual ~TunnelMan(){}
+};
+
+
+
+
+#endif // ACTOR_H_
