fe4b6d0933295eaad8549844238aae23e13cd440
shaniet <101319495+shaniet@users.noreply.github.com>
2022-08-21 18:32:05 -0700

    Update StudentWorld.h

--- Projects/TunnelMan/StudentWorld.h
+++ Projects/TunnelMan/StudentWorld.h
@@ -16,29 +16,44 @@ class StudentWorld : public GameWorld
 {
 public:
     StudentWorld(std::string assetDir):GameWorld(assetDir){}
-
+//TODO: set players to dead on grid
     virtual int init();
     virtual int move();
     virtual void cleanUp();
-    void removeEarth(int x, int y);
-   
-    void validCoordinate(int startx, int endx, int starty, int endy, int *x, int *y);
     void updateDisplayText();
+    void removeEarth(int x, int y);
+    void validCoordinate(int startx, int endx, int starty, int endy, int *x, int *y);
+    void emptyEarth(int startx, int endx, int starty, int endy, int &x, int &y);
     bool checkBoulder(int x, int y) const;
     void addActor(Actor* a);
     bool checkField(int x, int y, int state);
+    bool getTo(int x, int y, int state);
+    void setGrid(int x, int y, int val){
+        grid[x][y] = val;
+    }
+    void setNumProt(int num){
+        numProt = num;
+    }
+    int getNumProt(){
+        return numProt;
+    }
+    int getGrid(int x, int y){
+        return grid[x][y];
+    }
+    void simulateProtestor();
     TunnelMan *getTunnelMan() const{
         return m_tunnelMan;
     }
-    void illuminate(int x, int y);
+
     virtual ~StudentWorld();
 private:
     TunnelMan* m_tunnelMan; // pointer to the player
-	std::vector<Actor*> m_actors; // vector of pointers to all the actors in the game
+    std::vector<Actor*> m_actors; // vector of pointers to all the actors in the game
     Earth * m_earth[64][64] = {nullptr}; // 2D array of pointers to the earth objects
-    // int grid[64][64]; // 2D array of integers to store the contents of the grid
+     int grid[64][64]; // 2D array of integers to store the contents of the grid
     int numTicks; // number of ticks since the game started
     int numBarrelsLeft; // number of barrels left to be collected
+    int numProt;
 };
 
 #endif // STUDENTWORLD_H_
